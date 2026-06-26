5d35f740a5e33467110a4f8de8f26ab3a8f20201
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-06-26 11:30:01 +0500

    Add Firebase Auth import to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
