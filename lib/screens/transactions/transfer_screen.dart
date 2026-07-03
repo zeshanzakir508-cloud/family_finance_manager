b2149ae0e7da0675acc689026be7153aab6b02e6
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-03 19:52:38 +0500

    Add import for family_model in transfer_screen.dart

--- lib/screens/transactions/transfer_screen.dart
+++ lib/screens/transactions/transfer_screen.dart
@@ -6,7 +6,7 @@ import '../../providers/family_provider.dart';
 import '../../services/auth_service.dart';
 import '../../services/database_service.dart';
 import '../../models/transfer_model.dart';
-import '../../models/family_model.dart';
+import '../../models/family_model.dart';  // <-- ADD THIS IMPORT
 import '../../utils/app_theme.dart';
 import '../../utils/helpers.dart';
 
