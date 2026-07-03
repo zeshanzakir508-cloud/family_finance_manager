590f98796041c1de06919bdb586e14cd730dbc25
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-03 19:51:21 +0500

    Add family_model import and update DropdownMenuItem type
    Added import for family_model and updated DropdownMenuItem type.

--- lib/screens/transactions/add_expense_screen.dart
+++ lib/screens/transactions/add_expense_screen.dart
@@ -7,6 +7,7 @@ import '../../providers/family_provider.dart';
 import '../../services/auth_service.dart';
 import '../../services/database_service.dart';
 import '../../models/transaction_model.dart';
+import '../../models/family_model.dart';  // <-- ADD THIS IMPORT
 import '../../utils/app_theme.dart';
 import '../../utils/helpers.dart';
 
@@ -433,7 +434,7 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
               ),
               if (members.isNotEmpty)
                 ...members.map((member) {
-                  return DropdownMenuItem(
+                  return DropdownMenuItem<String>(
                     value: member.id,
                     child: Text(member.displayName),
                   );
