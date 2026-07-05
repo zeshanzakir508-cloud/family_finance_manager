819d669dd90b0dc0db3de644e0a234d640e0689f
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-05 10:13:46 +0500

    Update setup.sh to generate Hive adapters first
    Rearranged build steps to generate Hive adapters before building the web release.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,15 @@
 # Install Flutter
 git clone https://github.com/flutter/flutter.git -b stable /tmp/flutter
 export PATH="$PATH:/tmp/flutter/bin"
+
+# Enable web
 flutter config --enable-web
+
+# Get dependencies
 flutter pub get
+
+# ✅ Generate Hive adapters FIRST
+flutter pub run build_runner build --delete-conflicting-outputs
+
+# Then build web
 flutter build web --release
