9ef167d732fe4c388378ef5d1fe770746038211a
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-07 17:14:49 +0500

    Add AppLocalizationsDelegate class
    Implement AppLocalizationsDelegate for localization support.

--- lib/localization/app_localizations_delegate.dart
+++ lib/localization/app_localizations_delegate.dart
@@ -1,1 +1,24 @@
+// lib/localization/app_localizations_delegate.dart
+import 'package:flutter/material.dart';
+import 'app_localizations.dart';
 
+class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+  const AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'ur'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    AppLocalizations localizations = AppLocalizations(locale);
+    await localizations.load();
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(covariant LocalizationsDelegate<AppLocalizations> old) {
+    return false;
+  }
+}
