ae0be1d5ea2f741b02dd1f9e06ffcc0a3e10db2b
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-07 17:14:28 +0500

    Update app_localizations.dart

--- lib/localization/app_localizations.dart
+++ lib/localization/app_localizations.dart
@@ -1,1 +1,61 @@
+// lib/localization/app_localizations.dart
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+class AppLocalizations {
+  final Locale locale;
+  late Map<String, String> _localizedStrings;
+
+  AppLocalizations(this.locale);
+
+  static AppLocalizations? of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+
+  Future<void> load() async {
+    String jsonString = await rootBundle.loadString(
+      'assets/translations/${locale.languageCode}.json',
+    );
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+    _localizedStrings = jsonMap.map((key, value) => MapEntry(key, value.toString()));
+  }
+
+  String translate(String key) {
+    return _localizedStrings[key] ?? key;
+  }
+
+  String tr(String key, [List<String>? args]) {
+    String text = _localizedStrings[key] ?? key;
+    if (args != null) {
+      for (int i = 0; i < args.length; i++) {
+        text = text.replaceAll('{$i}', args[i]);
+      }
+    }
+    return text;
+  }
+}
+
+class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'ur'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    AppLocalizations localizations = AppLocalizations(locale);
+    await localizations.load();
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(covariant LocalizationsDelegate<AppLocalizations> old) {
+    return false;
+  }
+}
