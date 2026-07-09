555e1183fabbc26a9d3d643fff0d23c2774b89c3
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-09 18:05:42 +0500

    Update custom_bottom_nav.dart

--- lib/widgets/common/custom_bottom_nav.dart
+++ lib/widgets/common/custom_bottom_nav.dart
@@ -1,1 +1,58 @@
+// lib/widgets/common/custom_bottom_nav.dart
+import 'package:flutter/material.dart';
 
+class BottomNavItem {
+  final IconData icon;
+  final String label;
+
+  const BottomNavItem({
+    required this.icon,
+    required this.label,
+  });
+}
+
+class CustomBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+  final List<BottomNavItem> items;
+
+  const CustomBottomNav({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+    required this.items,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: isDark ? Colors.grey[900] : Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 10,
+            offset: const Offset(0, -2),
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: onTap,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: isDark ? Colors.grey[900] : Colors.white,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: isDark ? Colors.grey[500] : Colors.grey[600],
+        elevation: 0,
+        items: items.map((item) {
+          return BottomNavigationBarItem(
+            icon: Icon(item.icon),
+            label: item.label,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
