89709ad55f3b72292d018cd62239b0efb4205fac
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-09 19:50:47 +0500

    Update settings_tile.dart

--- lib/screens/settings/widgets/settings_tile.dart
+++ lib/screens/settings/widgets/settings_tile.dart
@@ -1,1 +1,75 @@
+// lib/screens/settings/widgets/settings_tile.dart
+import 'package:flutter/material.dart';
 
+class SettingsTile extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String? subtitle;
+  final Widget? trailing;
+  final VoidCallback? onTap;
+  final Color? iconColor;
+  final bool isDestructive;
+
+  const SettingsTile({
+    Key? key,
+    required this.icon,
+    required this.title,
+    this.subtitle,
+    this.trailing,
+    this.onTap,
+    this.iconColor,
+    this.isDestructive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final color = isDestructive ? Colors.red : (iconColor ?? Theme.of(context).primaryColor);
+
+    return Card(
+      elevation: 0,
+      color: isDark ? Colors.grey[800] : Colors.white,
+      margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 2),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: ListTile(
+        leading: Container(
+          width: 40,
+          height: 40,
+          decoration: BoxDecoration(
+            color: color.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Icon(
+            icon,
+            color: color,
+            size: 22,
+          ),
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            color: isDestructive ? Colors.red : null,
+          ),
+        ),
+        subtitle: subtitle != null
+            ? Text(
+                subtitle!,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: isDark ? Colors.grey[400] : Colors.grey[600],
+                ),
+              )
+            : null,
+        trailing: trailing ?? (onTap != null ? const Icon(Icons.chevron_right) : null),
+        onTap: onTap,
+        contentPadding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+        horizontalTitleGap: 12,
+        dense: true,
+      ),
+    );
+  }
+}
