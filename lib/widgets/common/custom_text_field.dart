44131b49c2f94f915ab3ae673a7fdc6b83cc5a3f
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-09 18:04:27 +0500

    Implement CustomTextField widget with various options

--- lib/widgets/common/custom_text_field.dart
+++ lib/widgets/common/custom_text_field.dart
@@ -1,1 +1,120 @@
+// lib/widgets/common/custom_text_field.dart
+import 'package:flutter/material.dart';
 
+class CustomTextField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String label;
+  final String? hint;
+  final IconData? prefixIcon;
+  final Widget? suffixIcon;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final TextInputAction textInputAction;
+  final String? Function(String?)? validator;
+  final void Function(String)? onChanged;
+  final void Function(String)? onFieldSubmitted;
+  final int maxLines;
+  final int? maxLength;
+  final bool enabled;
+  final bool readOnly;
+  final VoidCallback? onTap;
+  final FocusNode? focusNode;
+  final String? initialValue;
+  final TextStyle? style;
+
+  const CustomTextField({
+    Key? key,
+    this.controller,
+    required this.label,
+    this.hint,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.textInputAction = TextInputAction.next,
+    this.validator,
+    this.onChanged,
+    this.onFieldSubmitted,
+    this.maxLines = 1,
+    this.maxLength,
+    this.enabled = true,
+    this.readOnly = false,
+    this.onTap,
+    this.focusNode,
+    this.initialValue,
+    this.style,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return TextFormField(
+      controller: controller,
+      initialValue: controller == null ? initialValue : null,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      validator: validator,
+      onChanged: onChanged,
+      onFieldSubmitted: onFieldSubmitted,
+      maxLines: maxLines,
+      maxLength: maxLength,
+      enabled: enabled,
+      readOnly: readOnly,
+      onTap: onTap,
+      focusNode: focusNode,
+      style: style ??
+          TextStyle(
+            color: isDark ? Colors.white : Colors.black87,
+            fontSize: 14,
+          ),
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        prefixIcon: prefixIcon != null
+            ? Icon(prefixIcon, color: Colors.grey[600])
+            : null,
+        suffixIcon: suffixIcon,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: isDark ? Colors.grey[700]! : Colors.grey[300]!,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: Theme.of(context).primaryColor,
+            width: 2,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(
+            color: Colors.red,
+            width: 2,
+          ),
+        ),
+        filled: true,
+        fillColor: isDark ? Colors.grey[800] : Colors.white,
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 14,
+        ),
+        labelStyle: TextStyle(
+          color: isDark ? Colors.grey[400] : Colors.grey[700],
+          fontSize: 14,
+        ),
+        hintStyle: TextStyle(
+          color: isDark ? Colors.grey[500] : Colors.grey[400],
+          fontSize: 14,
+        ),
+        counterStyle: const TextStyle(fontSize: 12),
+      ),
+    );
+  }
+}
