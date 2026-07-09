03e6c570471273b797387ceaaef1c21c9fca8ccf
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-09 17:58:42 +0500

    Update loading_widget.dart

--- lib/widgets/common/loading_widget.dart
+++ lib/widgets/common/loading_widget.dart
@@ -1,1 +1,48 @@
+// lib/widgets/common/loading_widget.dart
+import 'package:flutter/material.dart';
 
+class LoadingWidget extends StatelessWidget {
+  final String? message;
+  final Color? color;
+  final double size;
+
+  const LoadingWidget({
+    Key? key,
+    this.message,
+    this.color,
+    this.size = 40,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final loadingColor = color ?? theme.primaryColor;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: size,
+            height: size,
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(loadingColor),
+              strokeWidth: 3,
+            ),
+          ),
+          if (message != null) ...[
+            const SizedBox(height: 16),
+            Text(
+              message!,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[600],
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+}
